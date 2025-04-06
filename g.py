1b5b72c561a42ab8110a23635f21d8993656438e
Arash-Khajavi <arash.khajavi.ghk123@gmail.com>
2025-04-06 19:56:13 +0300

    Update g.py

--- g.py
+++ g.py
@@ -27,4 +27,5 @@ def home():
     return "Welcome to my Flask app!"
 
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+       port = int(os.environ.get("PORT", 5000))  # Get Render's assigned port
+       app.run(host="0.0.0.0", port=port)
